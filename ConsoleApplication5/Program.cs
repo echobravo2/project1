5b32c6488201efc65ccd2cfc665697a33782cf9d
Alex Shapiro <alex.shapir@gmail.com>
2015-10-06 17:04:04 -0400

    modified message

--- ConsoleApplication5/Program.cs
+++ ConsoleApplication5/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hi Alex!");
+            Console.WriteLine("Hi Alex Shapiro!");
         }
     }
 }
